8581dc2a20dae2f6b5b233a176945f5167e5aaf8
Tonik Profik <izanami369@gmail.com>
2019-09-12 00:15:47 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board,index)
   board = ["", " ", " ", " ", " ", " ", " ", " ", ""]
-  index="X"
+  index=2
   if board[index] == " " or board[index] == "" or nil
     return false
   elsif board[index] == "X" or board[index] == "O"
